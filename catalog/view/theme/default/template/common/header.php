21bf845ffc31c4d724df1fc08d0f91cba353303d
web3d <web3d@live.com>
2016-09-17 11:48:41 +0800

    以header为例,演示将原控制器中视图相关代码直接交由视图处理的情形

--- catalog/view/theme/default/template/common/header.php
+++ catalog/view/theme/default/template/common/header.php
@@ -1,21 +1,31 @@
-<!DOCTYPE html>
+<?php 
+/* @var $this \Template\PHP */
+/* @var $url \Url */
+$url = $this->registry->get('url');
+/* @var $lang \Language */
+$lang = $this->registry->get('language');
+/* @var $loader \Loader */
+$loader = $this->registry->get('load');
+/* @var $document \Document */
+$document = $this->registry->get('document');
+?><!DOCTYPE html>
 <!--[if IE]><![endif]-->
-<!--[if IE 8 ]><html dir="<?php echo $direction; ?>" lang="<?php echo $lang; ?>" class="ie8"><![endif]-->
-<!--[if IE 9 ]><html dir="<?php echo $direction; ?>" lang="<?php echo $lang; ?>" class="ie9"><![endif]-->
+<!--[if IE 8 ]><html dir="<?php echo $lang->get('direction'); ?>" lang="<?php echo $lang->get('code'); ?>" class="ie8"><![endif]-->
+<!--[if IE 9 ]><html dir="<?php echo $lang->get('direction'); ?>" lang="<?php echo $lang->get('code'); ?>" class="ie9"><![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!-->
-<html dir="<?php echo $direction; ?>" lang="<?php echo $lang; ?>">
+<html dir="<?php echo $lang->get('direction'); ?>" lang="<?php echo $lang->get('code'); ?>">
 <!--<![endif]-->
 <head>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title><?php echo $title; ?></title>
+<title><?php echo $document->getTitle(); ?></title>
 <base href="<?php echo $base; ?>" />
-<?php if ($description) { ?>
-<meta name="description" content="<?php echo $description; ?>" />
+<?php if ($document->getDescription()) { ?>
+<meta name="description" content="<?php echo $document->getDescription(); ?>" />
 <?php } ?>
-<?php if ($keywords) { ?>
-<meta name="keywords" content= "<?php echo $keywords; ?>" />
+<?php if ($document->getKeywords()) { ?>
+<meta name="keywords" content= "<?php echo $document->getKeywords(); ?>" />
 <?php } ?>
 <script src="catalog/view/javascript/jquery/jquery-2.1.1.min.js" type="text/javascript"></script>
 <link href="catalog/view/javascript/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen" />
@@ -23,14 +33,14 @@
 <link href="catalog/view/javascript/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
 <link href="//fonts.googleapis.com/css?family=Open+Sans:400,400i,300,700" rel="stylesheet" type="text/css" />
 <link href="catalog/view/theme/default/stylesheet/stylesheet.css" rel="stylesheet">
-<?php foreach ($styles as $style) { ?>
+<?php foreach ($document->getStyles() as $style) { ?>
 <link href="<?php echo $style['href']; ?>" type="text/css" rel="<?php echo $style['rel']; ?>" media="<?php echo $style['media']; ?>" />
 <?php } ?>
 <script src="catalog/view/javascript/common.js" type="text/javascript"></script>
-<?php foreach ($links as $link) { ?>
+<?php foreach ($document->getLinks() as $link) { ?>
 <link href="<?php echo $link['href']; ?>" rel="<?php echo $link['rel']; ?>" />
 <?php } ?>
-<?php foreach ($scripts as $script) { ?>
+<?php foreach ($document->getScripts() as $script) { ?>
 <script src="<?php echo $script; ?>" type="text/javascript"></script>
 <?php } ?>
 <?php foreach ($analytics as $analytic) { ?>
@@ -40,28 +50,28 @@
 <body class="<?php echo $class; ?>">
 <nav id="top">
   <div class="container">
-    <?php echo $currency; ?>
-    <?php echo $language; ?>
+    <?php echo $loader->controller('common/currency'); ?>
+    <?php echo $loader->controller('common/language'); ?>
     <div id="top-links" class="nav pull-right">
       <ul class="list-inline">
-        <li><a href="<?php echo $contact; ?>"><i class="fa fa-phone"></i></a> <span class="hidden-xs hidden-sm hidden-md"><?php echo $telephone; ?></span></li>
-        <li class="dropdown"><a href="<?php echo $account; ?>" title="<?php echo $text_account; ?>" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <span class="hidden-xs hidden-sm hidden-md"><?php echo $text_account; ?></span> <span class="caret"></span></a>
+        <li><a href="<?php echo $url->link('information/contact'); ?>"><i class="fa fa-phone"></i></a> <span class="hidden-xs hidden-sm hidden-md"><?php echo $telephone; ?></span></li>
+        <li class="dropdown"><a href="<?php echo $url->link('account/account', '', true); ?>" title="<?php echo $lang->get('text_account'); ?>" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <span class="hidden-xs hidden-sm hidden-md"><?php echo $lang->get('text_account'); ?></span> <span class="caret"></span></a>
           <ul class="dropdown-menu dropdown-menu-right">
             <?php if ($logged) { ?>
-            <li><a href="<?php echo $account; ?>"><?php echo $text_account; ?></a></li>
-            <li><a href="<?php echo $order; ?>"><?php echo $text_order; ?></a></li>
-            <li><a href="<?php echo $transaction; ?>"><?php echo $text_transaction; ?></a></li>
-            <li><a href="<?php echo $download; ?>"><?php echo $text_download; ?></a></li>
-            <li><a href="<?php echo $logout; ?>"><?php echo $text_logout; ?></a></li>
+            <li><a href="<?php echo $url->link('account/account', '', true); ?>"><?php echo $lang->get('text_account'); ?></a></li>
+            <li><a href="<?php echo $url->link('account/order', '', true); ?>"><?php echo $lang->get('text_order'); ?></a></li>
+            <li><a href="<?php echo $url->link('account/transaction', '', true); ?>"><?php echo $lang->get('text_transaction'); ?></a></li>
+            <li><a href="<?php echo $url->link('account/download', '', true); ?>"><?php echo $lang->get('text_download'); ?></a></li>
+            <li><a href="<?php echo $url->link('account/logout', '', true); ?>"><?php echo $lang->get('text_logout'); ?></a></li>
             <?php } else { ?>
-            <li><a href="<?php echo $register; ?>"><?php echo $text_register; ?></a></li>
-            <li><a href="<?php echo $login; ?>"><?php echo $text_login; ?></a></li>
+            <li><a href="<?php echo $url->link('account/register', '', true); ?>"><?php echo $lang->get('text_register'); ?></a></li>
+            <li><a href="<?php echo $url->link('account/login', '', true); ?>"><?php echo $lang->get('text_login'); ?></a></li>
             <?php } ?>
           </ul>
         </li>
-        <li><a href="<?php echo $wishlist; ?>" id="wishlist-total" title="<?php echo $text_wishlist; ?>"><i class="fa fa-heart"></i> <span class="hidden-xs hidden-sm hidden-md"><?php echo $text_wishlist; ?></span></a></li>
-        <li><a href="<?php echo $shopping_cart; ?>" title="<?php echo $text_shopping_cart; ?>"><i class="fa fa-shopping-cart"></i> <span class="hidden-xs hidden-sm hidden-md"><?php echo $text_shopping_cart; ?></span></a></li>
-        <li><a href="<?php echo $checkout; ?>" title="<?php echo $text_checkout; ?>"><i class="fa fa-share"></i> <span class="hidden-xs hidden-sm hidden-md"><?php echo $text_checkout; ?></span></a></li>
+        <li><a href="<?php echo $url->link('account/wishlist', '', true); ?>" id="wishlist-total" title="<?php echo $text_wishlist; ?>"><i class="fa fa-heart"></i> <span class="hidden-xs hidden-sm hidden-md"><?php echo $text_wishlist; ?></span></a></li>
+        <li><a href="<?php echo $url->link('checkout/cart'); ?>" title="<?php echo $lang->get('text_shopping_cart'); ?>"><i class="fa fa-shopping-cart"></i> <span class="hidden-xs hidden-sm hidden-md"><?php echo $lang->get('text_shopping_cart'); ?></span></a></li>
+        <li><a href="<?php echo $url->link('checkout/checkout', '', true); ?>" title="<?php echo $lang->get('text_checkout'); ?>"><i class="fa fa-share"></i> <span class="hidden-xs hidden-sm hidden-md"><?php echo $lang->get('text_checkout'); ?></span></a></li>
       </ul>
     </div>
   </div>
@@ -72,22 +82,22 @@
       <div class="col-sm-4">
         <div id="logo">
           <?php if ($logo) { ?>
-          <a href="<?php echo $home; ?>"><img src="<?php echo $logo; ?>" title="<?php echo $name; ?>" alt="<?php echo $name; ?>" class="img-responsive" /></a>
+          <a href="<?php echo $url->link('common/home'); ?>"><img src="<?php echo $logo; ?>" title="<?php echo $name; ?>" alt="<?php echo $name; ?>" class="img-responsive" /></a>
           <?php } else { ?>
-          <h1><a href="<?php echo $home; ?>"><?php echo $name; ?></a></h1>
+          <h1><a href="<?php echo $url->link('common/home'); ?>"><?php echo $name; ?></a></h1>
           <?php } ?>
         </div>
       </div>
-      <div class="col-sm-5"><?php echo $search; ?>
+      <div class="col-sm-5"><?php echo $loader->controller('common/search'); ?>
       </div>
-      <div class="col-sm-3"><?php echo $cart; ?></div>
+      <div class="col-sm-3"><?php echo $loader->controller('common/cart'); ?></div>
     </div>
   </div>
 </header>
 <?php if ($categories) { ?>
 <div class="container">
   <nav id="menu" class="navbar">
-    <div class="navbar-header"><span id="category" class="visible-xs"><?php echo $text_category; ?></span>
+    <div class="navbar-header"><span id="category" class="visible-xs"><?php echo $lang->get('text_category'); ?></span>
       <button type="button" class="btn btn-navbar navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse"><i class="fa fa-bars"></i></button>
     </div>
     <div class="collapse navbar-collapse navbar-ex1-collapse">
@@ -105,7 +115,7 @@
               </ul>
               <?php } ?>
             </div>
-            <a href="<?php echo $category['href']; ?>" class="see-all"><?php echo $text_all; ?> <?php echo $category['name']; ?></a> </div>
+            <a href="<?php echo $category['href']; ?>" class="see-all"><?php echo $lang->get('text_all'); ?> <?php echo $category['name']; ?></a> </div>
         </li>
         <?php } else { ?>
         <li><a href="<?php echo $category['href']; ?>"><?php echo $category['name']; ?></a></li>
